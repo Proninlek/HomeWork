128180050396d178ee0e77ea426b3d8826602c3e
DFIT Vasia <pronin_lek@mail.ru>
2023-01-14 17:32:11 +0300

    Practik 3 Удачная попытка

--- Practik 3/Program.cs
+++ Practik 3/Program.cs
@@ -1,4 +1,11 @@
 ﻿Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 int numX = num % 2;
-Console.Write(numX);
+if(numX > 0)
+{
+    Console.WriteLine("Это нечетное число");
+}
+else
+{
+    Console.WriteLine("Верно, это четное число");
+}
