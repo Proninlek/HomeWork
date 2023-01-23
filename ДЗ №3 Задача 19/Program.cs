514b881769f4dfd37afc9ee488cd99c76a5cc48b
DFIT Vasia <pronin_lek@mail.ru>
2023-01-23 23:15:57 +0300

    задача 19 готова

--- ДЗ №3 Задача 19/Program.cs
+++ ДЗ №3 Задача 19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пяти значное число: ");
+int number = int.Parse(Console.ReadLine());
+int a1 = number % 100;
+int a2 = number / 1000 % 10;
+int a3 = number % 10;
+int a4 = number / 10000;
+if ((a1 == a2) || (a3 == a4))
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+   Console.WriteLine($"К сожалению, число {number} не является палиндромом");
+}
