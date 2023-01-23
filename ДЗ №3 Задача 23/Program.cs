d97ecb49610c3bca4e757c1ac7476d177fa75de6
DFIT Vasia <pronin_lek@mail.ru>
2023-01-23 22:38:34 +0300

    задача 23 готово

--- ДЗ №3 Задача 23/Program.cs
+++ ДЗ №3 Задача 23/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse (Console.ReadLine()!);
+int count = 1;
+while (count <= number)
+{
+    Console.Write($"{Math.Pow (count,3)}");
+    if (count != number)
+    Console.Write(", ");
+    count++;
+}
