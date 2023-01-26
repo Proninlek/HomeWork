800966c54f59920a154be2fddc51608634fd7041
DFIT Vasia <pronin_lek@mail.ru>
2023-01-27 01:17:18 +0300

    задача 25

---
+++ ДЗ №4 Задача 25/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите функцию, которая принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//Нельзя использовать библиотеку Math!
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+int Mult(int k, int p)
+{
+    int result = 1;
+    for(int i=1; i <= p; i++)
+    {
+    result = result * k;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numberA = int.Parse (Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = int.Parse (Console.ReadLine()!);
+int total = Mult(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {total}");
