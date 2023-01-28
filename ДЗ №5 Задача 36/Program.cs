064b2104f31d3247bcc91335cb334a8e3033eb20
DFIT Vasia <pronin_lek@mail.ru>
2023-01-28 21:59:00 +0300

    задача 36

---
+++ ДЗ №5 Задача 36/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] massiv = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        massiv[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return massiv;
+}
+
+int SumPosition(int[] number)
+{
+    int sum = 0;
+    for (int j = 1; j < number.Length; j+=2)
+    {
+        sum = sum + number[j];
+    }
+    return sum;
+}
+
+Console.Clear();
+int[] ourArray = GetArray(6, -999, 999);
+int result = SumPosition(ourArray);
+Console.WriteLine(String.Join(", ", ourArray));
+Console.WriteLine($"Сумма чисел на нечётных позициях равна {result}");
