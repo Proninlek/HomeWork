5033fe26a57f050955af0c4cb532acf24dfc3ac4
DFIT Vasia <pronin_lek@mail.ru>
2023-02-02 23:02:11 +0300

    задача 41 более продуманное решение

--- ДЗ 6 Задача 41/Program.cs
+++ ДЗ 6 Задача 41/Program.cs
@@ -1,17 +1,55 @@
 ﻿//Пользователь вводит с клавиатуры M чисел. 
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
-//1, -7, 567, 89, 223-> 3
+//-1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите элементы: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
+int[] StringToNum(string input)
 {
-    if (arr[i] > 0)
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
     {
-        count++;
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+        while (input [i] != ',')
+        {
+            if(i != input.Length - 1)
+            {
+                temp += input [i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input [i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+Console.Clear();
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
     }
 }
-Console.WriteLine($"Количество элементов больше 0 равно {count}");
+
+Console.WriteLine($"Количество значений больше нуля равно {sum}");
