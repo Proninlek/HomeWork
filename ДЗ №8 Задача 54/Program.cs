0c028b13e5f555432a77ee5824d430b40d9269af
DFIT Vasia <pronin_lek@mail.ru>
2023-02-11 19:10:03 +0300

    задача 54 Есть!

---
+++ ДЗ №8 Задача 54/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит
+// по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NewSortMassiv (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, col, 0, 10);
+PrintArray(array);
+Console.Write($"Отсортированный массив");
+Console.WriteLine();
+NewSortMassiv(array);
+PrintArray(array);
