ae8e6e00852e6e0d57f3637e10558711e927a68e
DFIT Vasia <pronin_lek@mail.ru>
2023-02-12 15:01:39 +0300

    задача 66 итоговая

---
+++ ДЗ №9 Задача 66/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int NumberSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NumberSum(m, n - 1);
+}
+
+void Main ()
+{
+Console.Clear();
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма элементов от {m} до {n} равна {NumberSum(m, n)}");
+}
+Main();
